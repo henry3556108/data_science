68fb8291d1a54a061fe818058f9d2bd8932e98b0
105590003 <henry3556108@gmail.com>
2021-01-06 02:01:40 +0800

    Add feature get date and add this feature in get_pcr

--- scraper/scraper_future_info/data_center.py
+++ scraper/scraper_future_info/data_center.py
@@ -4,7 +4,10 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import random
+import datetime
 # from seleniumrequests import Chrome
+
+
 class Data_center():
     def __init__(self):
         self.pcr_url = "https://www.taifex.com.tw/cht/3/pcRatio"
@@ -13,44 +16,66 @@ class Data_center():
         self.txf_url = ""
         self.browser = None
 
-    def init_browser(self, hide = True):
+    def init_browser(self, hide=True):
         options = webdriver.ChromeOptions()
-        if hide :
+        if hide:
             options.add_argument("--headless")
         agent = f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.{random.randint(0, 255)} Safari/537.36"
         options.add_argument('user-agent={}'.format(agent))
         self.browser = webdriver.Chrome(options=options)
 
+    def _get_date(self, start_date: str, end_date: str):
+        start_date = datetime.datetime.strptime(start_date, "%Y/%m/%d")
+        end_date = datetime.datetime.strptime(end_date, "%Y/%m/%d")
+        date_delta = (end_date - start_date).days
+        dates = []
+        if date_delta == 0:
+            dates = [(f"{start_date.year}/{start_date.month}/{start_date.day}",
+                      f"{start_date.year}/{start_date.month}/{start_date.day}")]
+        while date_delta > 0:
+            tmp_delta = 30 if date_delta >= 30 else date_delta
+            # print()
+            current_end = start_date + datetime.timedelta(tmp_delta)
+
+            dates.append((f"{start_date.year}/{start_date.month}/{start_date.day}",
+                          f"{current_end.year}/{current_end.month}/{current_end.day}"))
+
+            start_date += datetime.timedelta(tmp_delta + 1)
+            date_delta -= (tmp_delta + 1)
+        return dates
+
     def get_pcr(self):
         self.init_browser()
         self.browser.get(self.pcr_url)
-        start_tag = self.browser.find_element_by_name("queryStartDate")
-        end_tag = self.browser.find_element_by_name("queryEndDate")
+        pcr_df = pd.DataFrame()
+        dates = self._get_date("2010/1/1", "2010/12/31")
+        for start, end in dates:
+            start_tag = self.browser.find_element_by_name("queryStartDate")
+            end_tag = self.browser.find_element_by_name("queryEndDate")
+            start_tag.clear()
+            end_tag.clear()
+            start_tag.send_keys(start)
+            end_tag.send_keys(end)
+
+            send_buttom = self.browser.find_element_by_name("button3")
+            send_buttom.click()
+            html = self.browser.execute_script(
+                "return document.documentElement.outerHTML")
+            soup = BeautifulSoup(html)
+            table = soup.find("table", {"class": "table_a"})
+            df = pd.read_html(table.prettify())
+            pcr_df = pd.concat([pcr_df, df[0]])
+            # print(df[0].head())
+        pcr_df["日期"] = pcr_df["日期"].apply(lambda x : pd.to_datetime(x, format="%Y/%m/%d"))
+        pcr_df = pcr_df.sort_values("日期")
+        pcr_df = pcr_df.reset_index(drop = True)
+        return pcr_df
         
-        # dates = _get_dates()
 
-        start_tag.clear()
-        end_tag.clear()
-        start_tag.send_keys("2010/9/1")
-        end_tag.send_keys("2010/9/30")
-        
-        send_buttom = self.browser.find_element_by_name("button3")
-        send_buttom.click()
-        html = self.browser.execute_script("return document.documentElement.outerHTML")
-        soup = BeautifulSoup(html)
-        table = soup.find("table", {"class": "table_a"})
-        df = pd.read_html(table.prettify())
-        df[0].head()
-        print()
-
-
-def get_data(file_name : str) -> pd.DataFrame:
+def get_data(file_name: str) -> pd.DataFrame:
     if file_name.endswith(".csv"):
         df = pd.read_csv(file_name)
     return df
-
-
-
 
 
 if __name__ == "__main__":
--- scraper/scraper_future_info/test_data_center.py
+++ scraper/scraper_future_info/test_data_center.py
@@ -5,7 +5,18 @@ def test_get_pcr(dc):
     dc.get_pcr()
     # pass
 
+def test_get_date(dc):
+    dates = dc._get_date("2010/1/1", "2010/1/1")
+    print(dates)
+    dates = dc._get_date("2010/1/1", "2010/1/2")
+    print(dates)
+    dates = dc._get_date("2010/1/1", "2010/12/31")
+    print(dates)
+    dates = dc._get_date("2010/1/1", "2010/3/5")
+    print(dates)
+    
 
 if __name__ == "__main__":
     dc = data_center.Data_center()
-    test_get_pcr(dc)
+    # test_get_pcr(dc)
+    test_get_date(dc)
